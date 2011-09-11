784d3fa6cba025112fddb496c92b85c4f8fc6b79
daimrod <daimrod@gmail.com>
2011-09-11 18:38:13 +0200

    Add some custom configuration

--- custom.el
+++ custom.el
@@ -3,7 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("c00ae283980ab93afb798ae9e7a6d184c431f8c0" default))))
+ '(custom-safe-themes (quote ("c00ae283980ab93afb798ae9e7a6d184c431f8c0" default)))
+ '(safe-local-variable-values (quote ((readtable . runes) (Readtable . GLISP) (Package . SGML) (Package . LALR) (Syntax . Common-Lisp)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
