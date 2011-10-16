847e4d56efe23e48b3bee66f6dcd892747ccd2b0
daimrod <daimrod@gmail.com>
2011-10-16 12:03:44 +0200

    Use smarttabs and smart-tabs-mode in cc-mode.

--- config-cc-mode.el
+++ config-cc-mode.el
@@ -1,9 +1,16 @@
 ;;;; config-cc-mode.el
 
-(setq c-basic-offset 4
-      c-default-style '((java-mode . "java")
-                        (awk-mode . "awk")
-                        (c-mode . "k&r")
-                        (other . "linux")))
+(require 'smart-tabs-mode)
+(require 'smarttabs)
+
+(setq c-default-style
+      '((java-mode . "java")
+        (awk-mode . "awk")
+        (c-mode . "k&r")
+        (other . "linux")))
+
+(setq-default c-basic-offset 4)
+(setq-default tab-width 4) ; or any other preferred value
+(setq cua-auto-tabify-rectangles nil)
 
 (provide 'config-cc-mode)
