b2cf9cee820f03dfaebe3d8a9d41ce66976bdeff
daimrod <daimrod@gmail.com>
2011-10-16 12:02:50 +0200

    Enable BBDB in Gnus.

--- config-bbdb.el
+++ config-bbdb.el
@@ -3,41 +3,45 @@
 (setq bbdb-file (concat dotfiles-dir "bbdb")) ;; keep ~/ clean; set before loading
 
 (require 'bbdb)
-(bbdb-initialize)
+(bbdb-initialize 'gnus 'w3)
 
-(setq 
-    bbdb-offer-save 1                        ;; 1 means save-without-asking
+(add-hook 'gnus-startup-hook 'bbdb-insinuate-gnus)
+(bbdb-insinuate-w3)
 
-    
-    bbdb-use-pop-up t                        ;; allow popups for addresses
-    bbdb-electric-p t                        ;; be disposable with SPC
-    bbdb-popup-target-lines  1               ;; very small
-    
-    bbdb-dwim-net-address-allow-redundancy t ;; always use full name
-    bbdb-quiet-about-name-mismatches 2       ;; show name-mismatches 2 secs
+(setq
+ bbdb-offer-save 1                        ;; 1 means save-without-asking
 
-    bbdb-always-add-address t                ;; add new addresses to existing...
-                                             ;; ...contacts automatically
-    bbdb-canonicalize-redundant-nets-p t     ;; x@foo.bar.cx => x@bar.cx
+ gnus-score-find-score-files-function
+ '(gnus-score-find-bnews bbdb/gnus-score)
 
-    bbdb-completion-type nil                 ;; complete on anything
+ bbdb-use-pop-up t                        ;; allow popups for addresses
+ bbdb-electric-p t                        ;; be disposable with SPC
+ bbdb-popup-target-lines  1               ;; very small
 
-    bbdb-complete-name-allow-cycling t       ;; cycle through matches
-                                             ;; this only works partially
+ bbdb-dwim-net-address-allow-redundancy t ;; always use full name
+ bbdb-quiet-about-name-mismatches 2       ;; show name-mismatches 2 secs
 
-    bbbd-message-caching-enabled t           ;; be fast
-    bbdb-use-alternate-names t               ;; use AKA
+ bbdb-always-add-address t                ;; add new addresses to existing...
+ ;; ...contacts automatically
+ bbdb-canonicalize-redundant-nets-p t     ;; x@foo.bar.cx => x@bar.cx
 
+ bbdb-completion-type nil                 ;; complete on anything
 
-    bbdb-elided-display t                    ;; single-line addresses
+ bbdb-complete-name-allow-cycling t       ;; cycle through matches
+ ;; this only works partially
 
-    ;; auto-create addresses from mail
-    bbdb/mail-auto-create-p 'bbdb-ignore-some-messages-hook   
-    bbdb-ignore-some-messages-alist ;; don't ask about fake addresses
-    ;; NOTE: there can be only one entry per header (such as To, From)
-    ;; http://flex.ee.uec.ac.jp/texi/bbdb/bbdb_11.html
+ bbbd-message-caching-enabled t           ;; be fast
+ bbdb-use-alternate-names t               ;; use AKA
 
-    '(( "From" . "no.?reply\\|DAEMON\\|daemon\\|facebookmail\\|twitter")))
+ bbdb-elided-display t                    ;; single-line addresses
+
+ ;; auto-create addresses from mail
+ bbdb/mail-auto-create-p 'bbdb-ignore-some-messages-hook
+ bbdb-ignore-some-messages-alist ;; don't ask about fake addresses
+ ;; NOTE: there can be only one entry per header (such as To, From)
+ ;; http://flex.ee.uec.ac.jp/texi/bbdb/bbdb_11.html
+
+ '(( "From" . "no.?reply\\|DAEMON\\|daemon\\|facebookmail\\|twitter")))
 
 (provide 'config-bbdb)
 
