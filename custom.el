673dfe750e283c8fed2f7500b37e5a1a68e629f2
daimrod <daimrod@gmail.com>
2011-10-22 17:53:25 +0200

    misc custom

--- custom.el
+++ custom.el
@@ -4,7 +4,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes (quote ("c00ae283980ab93afb798ae9e7a6d184c431f8c0" default)))
- '(safe-local-variable-values (quote ((Syntax . ANSI-Common-Lisp) (Base . 10) (readtable . runes) (Readtable . GLISP) (Package . SGML) (Package . LALR) (Syntax . Common-Lisp)))))
+ '(safe-local-variable-values (quote ((Syntax . ANSI-Common-Lisp) (Base . 10) (readtable . runes) (Readtable . GLISP) (Package . SGML) (Package . LALR) (Syntax . Common-Lisp))))
+ '(send-mail-function (quote sendmail-send-it)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
