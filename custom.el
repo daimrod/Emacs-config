1d4fc472dc63c97f23bc01d5170788c988b71d28
daimrod <daimrod@gmail.com>
2011-10-28 18:00:30 +0200

    custom update

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(canlock-password "29c3867e8e33050380c126e46efaaa3445000d33")
  '(custom-safe-themes (quote ("c00ae283980ab93afb798ae9e7a6d184c431f8c0" default)))
  '(safe-local-variable-values (quote ((Syntax . ANSI-Common-Lisp) (Base . 10) (readtable . runes) (Readtable . GLISP) (Package . SGML) (Package . LALR) (Syntax . Common-Lisp))))
  '(send-mail-function (quote sendmail-send-it)))
