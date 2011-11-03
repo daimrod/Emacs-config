110f276b7d4a5d86ef91e5641e988f6efbf5065b
daimrod <daimrod@gmail.com>
2011-11-03 15:23:45 +0100

    Customize whitespace-mode.
    Show trailing white spaces, and when there is more than two line at
    the begining or the end of a buffer.

--- custom.el
+++ custom.el
@@ -5,11 +5,20 @@
  ;; If there is more than one, they won't work right.
  '(canlock-password "29c3867e8e33050380c126e46efaaa3445000d33")
  '(custom-safe-themes (quote ("c00ae283980ab93afb798ae9e7a6d184c431f8c0" default)))
+ '(global-whitespace-mode t)
+ '(global-whitespace-newline-mode t)
  '(ido-default-buffer-method (quote selected-window))
  '(ido-default-file-method (quote selected-window))
  '(safe-local-variable-values (quote ((Syntax . ANSI-Common-Lisp) (Base . 10) (readtable . runes) (Readtable . GLISP) (Package . SGML) (Package . LALR) (Syntax . Common-Lisp))))
  '(send-mail-function (quote sendmail-send-it))
- '(show-trailing-whitespace t))
+ '(whitespace-empty (quote whitespace-empty))
+ '(whitespace-empty-at-bob-regexp "^\\(\\([ 	]*
+
+\\)+\\)")
+ '(whitespace-empty-at-eob-regexp "^\\([ 	
+]\\{2,\\}\\)")
+ '(whitespace-style (quote (face trailing space-before-tab lines-tail empty))))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
