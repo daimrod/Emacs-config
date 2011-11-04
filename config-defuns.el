80f6e25f44c66ee7c328656b033914c8b6a818c4
daimrod <daimrod@gmail.com>
2011-11-04 10:30:26 +0100

    Do not recompile every files each times, but only those who need it.
    This is done by reading the documentation of byte-recompile-directory.
    (Auctex was way too big to be compiled every times)

--- config-defuns.el
+++ config-defuns.el
@@ -89,7 +89,7 @@ Symbols matching the text at point are put first in the completion list."
   (map nil #'byte-compile-file
        '("~/.gnus.el"
          "~/.emacs-w3m.el"))
-  (byte-recompile-directory dotfiles-dir 0 t))
+  (byte-recompile-directory dotfiles-dir))
 
 (defun move-to-window-line-top ()
   (interactive)
