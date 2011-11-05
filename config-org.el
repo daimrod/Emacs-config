26093a714d4ef524c980b7eec91d0975af4f0491
daimrod <daimrod@gmail.com>
2011-11-05 12:48:32 +0100

    I now use C-c o as a prefix to access to org mode globaly.
    For example:
    C-c o a to open the agenda
    C-c o l to store link
    etc...

--- config-org.el
+++ config-org.el
@@ -2,17 +2,21 @@
 
 (require 'org-install)
 
+;; Subcommands for org global keymap
+(define-prefix-command 'mode-specific-org-map)
+(define-key mode-specific-map (kbd "o") 'mode-specific-org-map)
+
 ;; define where org file should be located by default
 (setq org-directory "~/org/")
 
 ;; The following lines are always needed. Choose your own keys.
 (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
-(global-set-key (kbd "C-c l") 'org-store-link)
-(global-set-key (kbd "C-c a") 'org-agenda)
-(global-set-key (kbd "C-c b") 'org-iswitchb)
+(define-key mode-specific-org-map (kbd "l") 'org-store-link)
+(define-key mode-specific-org-map (kbd "a") 'org-agenda)
+(define-key mode-specific-org-map (kbd "b") 'org-iswitchb)
 
 ;; capture
-(global-set-key (kbd "C-c c") 'org-capture)
+(define-key mode-specific-org-map (kbd "c") 'org-capture)
 
 ;; define where notes should be stored (for capture with C-c c)
 (setq-default org-default-notes-file (concat org-directory "notes.org"))
