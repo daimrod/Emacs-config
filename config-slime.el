1d1970bdac55f4ba57e1e79013fce7a16a328572
daimrod <daimrod@gmail.com>
2011-11-06 11:27:00 +0100

    Auto Complete integrated with Slime

--- config-slime.el
+++ config-slime.el
@@ -65,4 +65,10 @@
 (make-directory "/tmp/slime-fasls/" t) ;; be sure the directory exists
 (setq slime-compile-file-options '(:fasl-directory "/tmp/slime-fasls/"))
 
+(require 'ac-slime)
+(add-hook 'slime-mode-hook 'set-up-slime-ac)
+(add-hook 'slime-repl-mode-hook 'set-up-slime-ac)
+(eval-after-load "auto-complete"
+  '(add-to-list 'ac-modes 'slime-repl-mode))
+
 (provide 'config-slime)
