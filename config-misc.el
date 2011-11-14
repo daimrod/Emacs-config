066229694e67cbb0bab9f31bf6a60c455b785541
daimrod <daimrod@gmail.com>
2011-11-14 16:40:51 +0100

    Zaps mode has been renamed to Markit mode

--- config-misc.el
+++ config-misc.el
@@ -109,9 +109,9 @@
 (setq-default major-mode 'text-mode)
 
 ;; enable Zaps mode everywhere
-(fni/add-to-load-path (concat src-dir "zaps/"))
-(require 'zaps)
-(global-zaps-mode t)
+(fni/add-to-load-path (concat src-dir "markit/"))
+(require 'markit)
+(global-markit-mode t)
 
 ;; yaml-mode configuration
 (fni/add-to-load-path (concat src-dir "yaml-mode/"))
