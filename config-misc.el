b9804c45f81fbc00a717d5e60270deeba641732d
daimrod <daimrod@gmail.com>
2011-11-21 21:59:33 +0100

    Add new repositories to ELPA and check them

--- config-misc.el
+++ config-misc.el
@@ -124,4 +124,9 @@
 ;; Autocompile el files at save
 (add-hook 'after-save-hook 'autocompile)
 
+;; ELPA configuration
+(setq package-archives '(("ELPA" . "http://tromey.com/elpa/") 
+                         ("gnu" . "http://elpa.gnu.org/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")))
+
 (provide 'config-misc)
