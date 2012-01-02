cde27d0ce0c6391ccebab94b5bdd762f401918bc
daimrod <daimrod@gmail.com>
2012-01-02 20:27:16 +0100

    Removed an alias

--- config-alias.el
+++ config-alias.el
@@ -16,7 +16,6 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-(defalias 'man 'woman)
 (defalias 'renb 'rename-buffer)
 
 (provide 'config-alias)
