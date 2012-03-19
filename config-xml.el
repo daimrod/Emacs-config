007022efa2d483411efd816b4aa5c8daf62904ea
daimrod <daimrod@gmail.com>
2012-03-19 17:18:32 +0100

    install and configure nxml-mode

---
+++ config-xml.el
@@ -0,0 +1,31 @@
+;;; config-xml.el ---
+
+;; Copyright (C) 2012 Grégoire Jadi
+
+;; Author: Grégoire Jadi <gregoire.jadi@gmail.com>
+
+;; This program is free software: you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation, either version 3 of
+;; the License, or (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'rng-auto)
+(setq auto-mode-alist
+      (cons '("\\.\\(xml\\|xsl\\|rng\\|xhtml\\)\\'" . nxml-mode)
+            auto-mode-alist))
+
+(provide 'config-xml)
+
+;;; config-xml.el ends here# -*- mode: snippet -*-
