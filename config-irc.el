658ee3b07ed2bd7fcbcffbfaf77fcc6449a78d79
daimrod <daimrod@gmail.com>
2012-04-01 02:22:21 +0200

    fixed fill-column

--- config-irc.el
+++ config-irc.el
@@ -27,6 +27,20 @@
 (fni/add-to-load-path (concat src-dir "znc/"))
 (require 'znc)
 
+(setq erc-timestamp-format "[%H:%M] "
+      erc-fill-prefix      "      + ")
+
+(make-variable-buffer-local 'erc-fill-column)
+(add-hook 'window-configuration-change-hook 
+          '(lambda ()
+             (save-excursion
+               (walk-windows
+                (lambda (w)
+                  (let ((buffer (window-buffer w)))
+                    (set-buffer buffer)
+                    (when (eq major-mode 'erc-mode)
+                      (setq erc-fill-column (- (window-width w) 2)))))))))
+
 (provide 'config-irc)
 
 ;;; config-irc.el ends here# -*- mode: snippet -*-
