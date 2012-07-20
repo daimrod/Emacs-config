daa85ce7f0886b3d51386e9d6b09711980bda928
Daimrod <daimrod@gmail>
2012-07-20 02:59:49 +0200

    fixed dmd/kill-emacs

--- config-defuns.el
+++ config-defuns.el
@@ -167,8 +167,7 @@ if it's the last frame, hide it, otherwise delete it.
 If not, use the classic save-buffers-and-kill-emacs function."
   (interactive)
   (if (boundp 'server-name)
-      (if (> (length server-clients) 1)
-          (delete-frame)
+      (unless (ignore-errors (delete-frame))
         (make-frame-invisible nil t))
     (save-buffers-kill-emacs)))
 
