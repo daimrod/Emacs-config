b33b0ea0e99133a6b506ea4c12407a5d7cd2c5e0
Grégoire Jadi <gregoire.jadi@gmail.com>
2012-09-25 18:48:24 +0200

    reinstalled markdown-mode via elpa

---
+++ elpa/markdown-mode-1.8.1/markdown-mode-autoloads.el
@@ -0,0 +1,29 @@
+;;; markdown-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (markdown-mode) "markdown-mode" "markdown-mode.el"
+;;;;;;  (20577 57357 0 0))
+;;; Generated autoloads from markdown-mode.el
+
+(autoload 'markdown-mode "markdown-mode" "\
+Major mode for editing Markdown files.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("markdown-mode-pkg.el") (20577 57357 334437
+;;;;;;  570000))
+
+;;;***
+
+(provide 'markdown-mode-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; markdown-mode-autoloads.el ends here
