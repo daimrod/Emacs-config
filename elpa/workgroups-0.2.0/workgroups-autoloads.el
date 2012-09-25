02a2e52357e553f92ced32367c0193417bd9044f
Grégoire Jadi <gregoire.jadi@gmail.com>
2012-09-25 18:37:07 +0200

    reinstalled workgroups via elpa

---
+++ elpa/workgroups-0.2.0/workgroups-autoloads.el
@@ -0,0 +1,18 @@
+;;; workgroups-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("workgroups-pkg.el" "workgroups.el") (20577
+;;;;;;  56727 373936 853000))
+
+;;;***
+
+(provide 'workgroups-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; workgroups-autoloads.el ends here
---
+++ elpa/workgroups-0.2.0/workgroups-pkg.el
@@ -0,0 +1,1 @@
+(define-package "workgroups" "0.2.0" "workgroups for windows (for Emacs)" (quote nil))
