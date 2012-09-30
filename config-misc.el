a4b8c18e168d6df1382500c791fe15651693b0eb
Grégoire Jadi <gregoire.jadi@gmail.com>
2012-09-30 12:59:26 +0200

    delete-selection enabled

--- config-misc.el
+++ config-misc.el
@@ -123,7 +123,7 @@
 (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
 
 ;; Enable delete-selection-mode
-(delete-selection-mode)
+(delete-selection-mode 1)
 
 ;; ELPA configuration
 (setq package-archives '(("ELPA" . "http://tromey.com/elpa/") 
