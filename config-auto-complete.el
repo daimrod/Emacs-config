1d2669a61d33bf3d2e25adb60e02d4dad7dac070
Grégoire Jadi <gregoire.jadi@gmail.com>
2012-10-08 18:02:46 +0200

    auto-complete is installed via ELPA

--- config-auto-complete.el
+++ config-auto-complete.el
@@ -16,11 +16,6 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-;; How to update auto-complete
-;; cd ~/src/elisp/auto-complete/
-;; git pull
-;; make install DIR=~/.emacs.d/elisp/
-
 (require 'auto-complete-config)
 
 (ac-config-default)
