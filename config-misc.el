cc69b16cde8a7d13d1890fe9fd7816292698a7ba
Grégoire Jadi <gregoire.jadi@gmail.com>
2012-11-20 23:45:07 +0100

    U_u Why didn't I install this earlier?

--- config-misc.el
+++ config-misc.el
@@ -202,4 +202,26 @@
 (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
 (global-set-key (kbd "C-c C-c C-M-x") 'execute-extended-command)
 
+;;; ace-jump-mode configuration
+(autoload
+  'ace-jump-mode
+  "ace-jump-mode"
+  "Emacs quick move minor mode"
+  t)
+;; you can select the key you prefer to
+(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
+
+;; 
+;; enable a more powerful jump back function from ace jump mode
+;;
+(autoload
+  'ace-jump-mode-pop-mark
+  "ace-jump-mode"
+  "Ace jump back:-)"
+  t)
+(eval-after-load "ace-jump-mode"
+  '(ace-jump-mode-enable-mark-sync))
+(define-key global-map (kbd "C-x SPC") 'ace-jump-mode-pop-mark)
+
+
 (provide 'config-misc)
