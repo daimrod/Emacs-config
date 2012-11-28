91838573eba2a6f559e4b48c4c523c03b0f73993
Grégoire Jadi <gregoire.jadi@gmail.com>
2012-11-28 01:04:35 +0100

    fix theme

--- config-theme.el
+++ config-theme.el
@@ -26,13 +26,8 @@
 (require 'color-theme-tangotango)
 (color-theme-tangotango)
 
-;; Set the default font
-(defvar *fonts-list* '("-unknown-Inconsolata-normal-normal-normal-*-18-*-*-*-m-0-iso10646-1"
-                       "-unknown-DejaVu Sans Mono-normal-normal-normal-*-15-*-*-*-m-0-iso10646-1"))
-
-(add-to-list 'default-frame-alist `(font . ,(find-if 'font-info *fonts-list*)))
-
-(setq initial-frame-alist (append initial-frame-alist (copy-alist default-frame-alist)))
+(custom-set-faces
+ '(default ((t (:inherit nil :stipple nil :background "#2e3434" :foreground "#eeeeec" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 135 :width normal :foundry "unknown" :family "Inconsolata")))))
 
 ;; tab and indentation configuration
 (setq-default indent-tabs-mode nil)
