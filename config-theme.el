731c7b954ae2bdc47e3a905918cf083701f5c05e
Grégoire Jadi <gregoire.jadi@gmail.com>
2012-11-29 23:25:48 +0100

    fix emacsc rather than my configuration.

--- config-theme.el
+++ config-theme.el
@@ -26,8 +26,12 @@
 (require 'color-theme-tangotango)
 (color-theme-tangotango)
 
-(custom-set-faces
- '(default ((t (:inherit nil :stipple nil :background "#2e3434" :foreground "#eeeeec" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 135 :width normal :foundry "unknown" :family "Inconsolata")))))
+;; Set the default font
+(defvar *fonts-list* '("-unknown-Inconsolata-normal-normal-normal-*-18-*-*-*-m-0-iso10646-1"
+                       "-unknown-DejaVu Sans Mono-normal-normal-normal-*-15-*-*-*-m-0-iso10646-1"))
+
+(add-to-list 'default-frame-alist `(font . ,(find-if #'font-info *fonts-list*)))
+(setq initial-frame-alist (append initial-frame-alist (copy-alist default-frame-alist)))
 
 ;; tab and indentation configuration
 (setq-default indent-tabs-mode nil)
