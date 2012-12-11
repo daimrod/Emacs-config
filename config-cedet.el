05a75b16f09a1bbb7eb0292aa38de8106c8a1f25
Grégoire Jadi <gregoire.jadi@gmail.com>
2012-12-11 15:38:12 +0100

    remove dead code

--- config-cedet.el
+++ config-cedet.el
@@ -28,7 +28,6 @@
         global-semantic-mru-bookmark-mode
         global-semantic-highlight-func-mode
         global-semantic-stickyfunc-mode
-        ;; global-semantic-decoration-mode
         global-semantic-idle-local-symbol-highlight-mode
         global-semantic-idle-scheduler-mode
         global-semantic-idle-summary-mode))
