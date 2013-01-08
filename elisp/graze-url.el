06a6390ebf665635418e4d183ede9dfc12d90af0
jgreg <gregoire.jadi@gmail.com>
2013-01-08 13:48:05 +0100

    Create a custom group for `graze.el', remove dependencies to `config-w3m' and thus `emacs-w3m' in `graze.el'.

--- elisp/graze-url.el
+++ elisp/graze-url.el
@@ -21,33 +21,58 @@
 
 ;;; Code:
 
-(require 'config-w3m)
 (require 'cl)
 
-(defvar gu-find-url-functions nil
-  "A list of functions used to find the url at point.")
+(defgroup graze-url nil
+  "Browse URL"
+  :group 'external)
 
-(setf gu-find-url-functions
-      '((lambda ()
-          (when (every 'fboundp
-                       '(org-at-regexp-p
-                         org-link-unescape
-                         org-match-string-no-properties))
-            (if (org-at-regexp-p org-bracket-link-regexp)
-                (org-link-unescape
-                 (org-match-string-no-properties 1)))))
-        (lambda ()
-          (when (every 'fboundp
-                       '(w3m-url-valid
-                         w3m-anchor))
-            (w3m-url-valid (w3m-anchor))))
-        (lambda ()
-          (get-text-property (point) 'shr-url))
-        (lambda ()
-          (when (fboundp 'thing-at-point)
-            (thing-at-point 'url)))))
+(defcustom gu-find-url-functions
+  '(gu-find-url-org-mode
+    gu-find-url-w3m
+    gu-find-url-text-property
+    gu-find-url-thing-at-point)
+  "A list of functions used to find the url at point."
+  :type '(repeat function))
+
+(defcustom gu-first-browse-url-function
+  'browse-url-browser-function
+  "The default function used to browse an URL."
+  :type 'function)
+
+(defcustom gu-second-browse-url-function
+  'browse-url-default-browser
+  "The other function used to browse an URL."
+  :type 'function)
+
+(defun gu-find-url-org-mode ()
+  "Find URL in `org-mode' style."
+  (when (every 'fboundp
+               '(org-at-regexp-p
+                 org-link-unescape
+                 org-match-string-no-properties))
+    (if (org-at-regexp-p org-bracket-link-regexp)
+        (org-link-unescape
+         (org-match-string-no-properties 1)))))
+
+(defun gu-find-url-w3m ()
+  "Find URL using `emacs-w3m' helper functions."
+  (when (and 
+         (fboundp 'w3m-url-valid)
+         (fboundp 'w3m-anchor))
+    (w3m-url-valid (w3m-anchor))))
+
+(defun gu-find-url-text-property ()
+  "Find URL stored in text property."
+  (get-text-property (point) 'shr-url))
+
+(defun gu-find-url-thing-at-point ()
+  "Find URL using `thing-at-point'."
+  (when (fboundp 'thing-at-point)
+    (thing-at-point 'url)))
 
 (defun gu-find-url-at-point ()
+  "Find URL using functions in `gu-find-url-functions'."
   (let (url)
     (loop for fun in gu-find-url-functions
           until (setf url (ignore-errors
@@ -62,22 +87,6 @@
         (error "No url found at point")
       (kill-new url)
       (message "%s" url))))
-
-(defun gu-w3m-browse-url-new-session (url)
-  "Ask emacs-w3m to browse URL."
-  (interactive)
-  (w3m-browse-url (w3m-canonicalize-url url) t))
-
-(defun gu-conkeror-browse-url (url)
-  "Ask conkeror to browse URL."
-  (interactive)
-  (start-process (concat "conkeror" url)
-                 nil
-                 "conkeror"
-                 url))
-
-(defvar *gu-first-browse-url-function* 'gu-w3m-browse-url-new-session)
-(defvar *gu-second-browse-url-function* 'gu-conkeror-browse-url)
 
 (defun gu-browse-url-interactive-arg (prompt)
   "Ask an URL with the given PROMPT."
@@ -94,16 +103,16 @@
 (defun gu-browse-url (url &optional second-function?)
   (interactive (gu-browse-url-interactive-arg "URL: "))
   (funcall (if second-function?
-               *gu-second-browse-url-function*
-             *gu-first-browse-url-function*)
+               gu-second-browse-url-function
+             gu-first-browse-url-function)
            url))
 
 (defun gu-search (term &optional second-function?)
   (interactive (list (read-string "Search: ")
                      current-prefix-arg))
   (funcall (if second-function?
-               *gu-second-browse-url-function*
-             *gu-first-browse-url-function*)
+               gu-second-browse-url-function
+             gu-first-browse-url-function)
            (format "http://s.s/search?q=%s" term)))
 
 (provide 'graze-url)
