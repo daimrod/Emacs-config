e888a2a1bbfc5d3036f4ad49306f217fb164a10f
Grégoire Jadi <gregoire.jadi@gmail.com>
2013-01-08 18:11:33 +0100

    fix formatting

--- elisp/bbdb-to-org-contacts.el
+++ elisp/bbdb-to-org-contacts.el
@@ -9,25 +9,22 @@
   "outputs a org-contacts file"
   (interactive (list (read-file-name "Save in file: ")))
   (let* ((filename (expand-file-name to-file))
-	 (records (bbdb-records)))
+         (records (bbdb-records)))
     (find-file filename)
     (while records
       (bbdb-record-to-org-record (car records))
-      (setq records (cdr records)))
-    ))
+      (setq records (cdr records)))))
 
-    
+
 (defun bbdb-record-to-org-record (record)
   "converts a single record"
-  (let* (
-         (name    (bbdb-record-name record))
+  (let* ((name    (bbdb-record-name record))
          (company (bbdb-record-company record))
          (net     (bbdb-record-net record))
          (aka     (bbdb-record-aka record))
          (phone   (bbdb-record-phones record))
          (address (bbdb-record-addresses record))
-         (notes   (bbdb-record-notes record))
-	 )
+         (notes   (bbdb-record-notes record)))
     
     (insert
      (format "%s %s\n" bbdb-to-org-contacts-record-prefix name))
@@ -37,12 +34,12 @@
     (when aka
       (insert
        (format "%s :AKA:\t%s\n" bbdb-to-org-contacts-record-blanks
-	       (mapconcat (function (lambda(str) str)) aka ", "))))
+               (mapconcat (function (lambda(str) str)) aka ", "))))
 
     (when net
       (insert
        (format "%s :EMAIL:\t%s\n" bbdb-to-org-contacts-record-blanks
-	       (mapconcat (function (lambda(str) str)) net " "))))
+               (mapconcat (function (lambda(str) str)) net " "))))
 
     (when company
       (insert
@@ -51,25 +48,25 @@
     (when phone
       (insert
        (mapconcat
-	(function (lambda(rec) 
-		    (if (stringp (elt rec 1))
-			(format "%s :PHONE_%s:\t%s"
-				bbdb-to-org-contacts-record-blanks
-				(upcase (elt rec 0))
-				(elt rec 1))
-		      (let ((len (length rec))
-			    (count 2)
-			    (output (format "%d" (elt rec 1))))
-			(while (< count len)
-			  (setq output
-				(concat output
-					(format "-%d"
-						(elt rec count))))
-			  (setq count (1+ count)))
-			(format "%s :PHONE_%s:\t%s"
-				bbdb-to-org-contacts-record-blanks
-				(upcase (elt rec 0)) output)))))
-	phone "\n"))
+        (function (lambda(rec) 
+                    (if (stringp (elt rec 1))
+                        (format "%s :PHONE_%s:\t%s"
+                                bbdb-to-org-contacts-record-blanks
+                                (upcase (elt rec 0))
+                                (elt rec 1))
+                      (let ((len (length rec))
+                            (count 2)
+                            (output (format "%d" (elt rec 1))))
+                        (while (< count len)
+                          (setq output
+                                (concat output
+                                        (format "-%d"
+                                                (elt rec count))))
+                          (setq count (1+ count)))
+                        (format "%s :PHONE_%s:\t%s"
+                                bbdb-to-org-contacts-record-blanks
+                                (upcase (elt rec 0)) output)))))
+        phone "\n"))
       (insert "\n"))
 
     (insert
@@ -77,9 +74,7 @@
 
     (when notes
       (insert
-       (format "%s - %s\n" bbdb-to-org-contacts-record-blanks notes)))
-
-    ))
+       (format "%s - %s\n" bbdb-to-org-contacts-record-blanks notes)))))
 
 
 (provide 'bbdb-to-org-contacts)
