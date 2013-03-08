c4baa3e8a0068e23197bb0c0e8a2a39ee6a86c16
Grégoire Jadi <gregoire.jadi@gmail.com>
2013-03-08 09:52:40 +0100

    config-misc.el: Fix `browse-url-function'

--- config-misc.el
+++ config-misc.el
@@ -83,7 +83,7 @@
       wg-query-for-save-on-emacs-exit nil
       wg-query-for-save-on-workgroups-mode-exit nil)
 
-(setq browse-url-browser-function 'w3m-browse-url)
+(setq browse-url-browser-function '(lambda (url) (w3m-browse-url url 'new-session)))
 
 ;; markdown configuration
 (autoload 'markdown-mode "markdown-mode"
