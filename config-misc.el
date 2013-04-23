1c00fd5c1d605d3b97c9b87948a9379a002af15b
Grégoire Jadi <gregoire.jadi@gmail.com>
2013-04-24 00:50:18 +0200

    fix order

--- config-misc.el
+++ config-misc.el
@@ -70,11 +70,6 @@
       ido-default-file-method 'selected-window
       ido-default-buffer-method 'selected-window)
 
-(fni/add-to-load-path (concat src-dir "ido-ubiquitous/"))
-(require 'ido-ubiquitous)
-(ido-ubiquitous-mode t)
-(setq ido-ubiquitous-enable-compatibility nil)
-
 ;; Display ido results vertically, rather than horizontally
 (setq ido-decorations '("\n-> "
                         ""
@@ -86,14 +81,21 @@
                         " [Not readable]"
                         " [Too big]"
                         " [Confirm]"))
+
 (defun ido-disable-line-truncation ()
   (set (make-local-variable 'truncate-lines) nil))
 (add-hook 'ido-minibuffer-setup-hook 'ido-disable-line-truncation)
 (defun ido-define-keys ()
   ;; C-n/p is more intuitive in vertical layout
+
   (define-key ido-completion-map (kbd "C-n") 'ido-next-match)
   (define-key ido-completion-map (kbd "C-p") 'ido-prev-match))
 (add-hook 'ido-setup-hook 'ido-define-keys)
+
+(fni/add-to-load-path (concat src-dir "ido-ubiquitous/"))
+(require 'ido-ubiquitous)
+(ido-ubiquitous-mode t)
+(setq ido-ubiquitous-enable-compatibility nil)
 
 (add-hook 'text-mode-hook 'turn-on-visual-line-mode)
 
