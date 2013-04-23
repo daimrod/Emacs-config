ce3129c52ce42a3c2fe50d895bb6aca04ad483d5
Grégoire Jadi <gregoire.jadi@gmail.com>
2013-04-24 00:23:10 +0200

    ido magic enabled

--- config-misc.el
+++ config-misc.el
@@ -75,6 +75,26 @@
 (ido-ubiquitous-mode t)
 (setq ido-ubiquitous-enable-compatibility nil)
 
+;; Display ido results vertically, rather than horizontally
+(setq ido-decorations '("\n-> "
+                        ""
+                        "\n   "
+                        "\n   ..."
+                        "[" "]"
+                        " [No match]"
+                        " [Matched]"
+                        " [Not readable]"
+                        " [Too big]"
+                        " [Confirm]"))
+(defun ido-disable-line-truncation ()
+  (set (make-local-variable 'truncate-lines) nil))
+(add-hook 'ido-minibuffer-setup-hook 'ido-disable-line-truncation)
+(defun ido-define-keys ()
+  ;; C-n/p is more intuitive in vertical layout
+  (define-key ido-completion-map (kbd "C-n") 'ido-next-match)
+  (define-key ido-completion-map (kbd "C-p") 'ido-prev-match))
+(add-hook 'ido-setup-hook 'ido-define-keys)
+
 (add-hook 'text-mode-hook 'turn-on-visual-line-mode)
 
 ;; workgroups configuration
