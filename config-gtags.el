b582b2f0dd4813c1723eea0a7001d2487e301c6f
Grégoire Jadi <gregoire.jadi@gmail.com>
2013-06-13 10:09:13 +0200

    * config-gtags.el: Add a new function to find a tag or a pattern depending on the prefix arg and bind it to M-.

--- config-gtags.el
+++ config-gtags.el
@@ -26,7 +26,16 @@
   (error "global executable not found, be sure GNU Global is installed and in your PATH."))
 
 (require 'gtags)
+(defun gtags-find (&optional with-grep?)
+  "Find a tag with `gtags-find-tag' if WITH-GREP? is nil,
+otherwise use `gtags-find-with-grep'."
+  (interactive "P")
+  (if with-grep?
+      (gtags-find-with-grep)
+    (gtags-find-tag)))
+
 (define-key gtags-mode-map (kbd "M-,") 'gtags-pop-stack)
+(define-key gtags-mode-map (kbd "M-.") 'gtags-find)
 
 (add-hook 'c-mode-hook '(lambda () (gtags-mode 1)))
 (add-hook 'c++-mode-hook '(lambda () (gtags-mode 1)))
