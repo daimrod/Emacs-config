941f5cbf0ef99f1a4175a249b75eecc08d8ffedd
Grégoire Jadi <gregoire.jadi@gmail.com>
2013-07-16 10:09:10 +0200

    * config-yasnippet.el: Change yas-trigger-key

--- config-yasnippet.el
+++ config-yasnippet.el
@@ -25,7 +25,7 @@
        (concat src-dir "yasnippet/snippets/")))
 
 ;; Load the snippets
-(setq yas-trigger-key "C-c SPC"
+(setq yas-trigger-key "M-c SPC"
       yas-fallback-behavior 'call-other-command
       yas-prompt-functions '(yas-ido-prompt))
 
