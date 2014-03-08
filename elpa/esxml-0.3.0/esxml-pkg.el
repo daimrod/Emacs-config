624cd407dee23d4a95f8aa03cad76b61049b715c
Grégoire Jadi <gregoire.jadi@gmail.com>
2014-03-08 10:46:34 +0900

    ELPA update

---
+++ elpa/esxml-0.3.0/esxml-pkg.el
@@ -0,0 +1,1 @@
+(define-package "esxml" "0.3.0" "Handle HTML with lists." '((db "0.0.1")))
---
+++ elpa/request-0.2.0/request-autoloads.el
@@ -0,0 +1,15 @@
+;;; request-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("request.el") (21269 44909 217294 341000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; request-autoloads.el ends here
