c3c8089cd68a8af0a2b48c1907361d9229811b5a
Grégoire Jadi <gregoire.jadi@gmail.com>
2014-08-27 20:24:59 +0200

    Disable horizontal scroll bar (available in new emacs)

--- init.el
+++ init.el
@@ -19,6 +19,7 @@
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(if (fboundp 'horizontal-scroll-bar-mode) (horizontal-scroll-bar-mode -1))
 
 ;; Load path etc.
 
