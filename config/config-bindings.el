9dc5feaf1259393c3a32b87e7e556057b7ca244b
Grégoire Jadi <gregoire.jadi@univ-nantes.fr>
2015-07-02 11:47:10 +0200

    m0ar for helm o/

--- config/config-bindings.el
+++ config/config-bindings.el
@@ -106,6 +106,8 @@
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "C-c m") 'helm-M-x)
 (global-set-key (kbd "C-c C-m") 'helm-M-x)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "C-x b") 'helm-buffers-list)
 
 ;;; EMMS
 (global-set-key (kbd "M-<f10>") 'emms-previous)
