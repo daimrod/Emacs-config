fcb9e1745ade317fc7977ee8765f994c2ce03452
Grégoire Jadi <gregoire.jadi@univ-nantes.fr>
2015-09-01 11:04:45 +0200

    * ELPA:chore: update ELPA's packages

--- elpa/dash-2.10.0/dash-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "dash" "2.10.0" "A modern list library for Emacs" 'nil)
---
+++ elpa/epl-0.8/epl-autoloads.el
@@ -0,0 +1,16 @@
+;;; epl-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("epl-pkg.el" "epl.el") (21984 10228 409350
+;;;;;;  79000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; epl-autoloads.el ends here
