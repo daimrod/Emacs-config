0e4975352efbfa87ee6375edae0c1c8690b355d7
Grégoire Jadi <gregoire.jadi@univ-nantes.fr>
2015-09-07 14:29:40 +0200

    feat(gist): Update gist

--- elpa/gist-1.2.1/gist-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "gist" "1.2.1" "Emacs integration for gist.github.com" '((eieio "1.4") (gh "0.8.1") (tabulated-list "0")))
---
+++ elpa/gist-1.3.1/gist-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "gist" "1.3.1" "Emacs integration for gist.github.com" '((emacs "24.1") (gh "0.9.2")))
