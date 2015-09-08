b95f67e06cdb35aab42bc05f9896a3cdec1d8ce3
Grégoire Jadi <gregoire.jadi@univ-nantes.fr>
2015-09-08 09:17:47 +0200

    feat(config-misc): Enable global flycheck-mode

--- config/config-misc.el
+++ config/config-misc.el
@@ -1,5 +1,6 @@
-;; config-misc.el
-;; Copyright (C) 2011, 2012 Grégoire Jadi
+;;; config-misc.el --- misc configuration
+
+;; Copyright (C) 2015 Grégoire Jadi
 
 ;; Author: Grégoire Jadi <gregoire.jadi@gmail.com>
 
@@ -15,6 +16,11 @@
 
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
+
 
 (require 'workgroups)
 (require 'scratch)
@@ -253,4 +259,8 @@
 (helm-mode 1)
 (helm-projectile-on)
 
+(global-flycheck-mode 1)
+
 (provide 'config-misc)
+
+;;; config-misc.el ends here
