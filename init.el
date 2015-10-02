1e44ec78e95f907b670c8941f5d23730bce5b089
Grégoire Jadi <gregoire.jadi@univ-nantes.fr>
2015-10-02 12:15:47 +0200

    feat(w3m): Jump to title in buffer. WIP
    I still need to find the right place to call this function, no hook seems to
    work.

--- init.el
+++ init.el
@@ -106,7 +106,17 @@
 
 
 (use-package w3m
-  :commands (w3m-buffer w3m-browse-url))
+  :commands (w3m-buffer w3m-browse-url)
+  :config
+  (defun dmd--w3m-go-to-title-in-page ()
+    (interactive)
+    (let ((title (w3m-buffer-title (current-buffer)))
+          (max-cut 10))
+      (when title
+        (goto-char (point-min))
+        (while (and (not (search-forward-regexp title nil t))
+                    (> (length title) max-cut)
+                    (setq title (subseq title 0 (1- (length title))))))))))
 
 
 (use-package windmove
