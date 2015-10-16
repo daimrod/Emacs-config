311352d349b5f99bb9cf472b725c71f3284290d5
Grégoire Jadi <gregoire.jadi@univ-nantes.fr>
2015-10-16 09:43:43 +0200

    chore(elisp): Byte compile even if the .elc doesn't exist.

--- elisp/.dir-locals.el
+++ elisp/.dir-locals.el
@@ -2,7 +2,7 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((emacs-lisp-mode
-  (firestarter . (byte-recompile-file (buffer-file-name)))))
+  (firestarter . (byte-recompile-file (buffer-file-name) nil 0))))
 
 ;; Local Variables:
 ;; no-byte-compile: t
