8e48e7b5ef76036f0b8b832911039633868dcf20
Grégoire Jadi <gregoire.jadi@univ-nantes.fr>
2015-11-10 14:35:57 +0100

    feat(autoloads): Generate some autoloads

---
+++ autoloads/graze-url-autoloads.el
@@ -0,0 +1,34 @@
+;;; graze-url-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil "../elisp/graze-url" "../elisp/graze-url.el"
+;;;;;;  (22081 62068 771906 659000))
+;;; Generated autoloads from ../elisp/graze-url.el
+
+(autoload 'gu-copy-url-at-point "../elisp/graze-url" "\
+Copy the url at point.
+
+\(fn)" t nil)
+
+(autoload 'gu-browse-url "../elisp/graze-url" "\
+
+
+\(fn &rest _)" t nil)
+
+(autoload 'gu-search "../elisp/graze-url" "\
+
+
+\(fn)" t nil)
+
+;;;***
+
+(provide 'graze-url-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; graze-url-autoloads.el ends here
