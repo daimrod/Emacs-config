0475e3d3606ba1ac7e7c1d29d20e98d6455417f2
Grégoire Jadi <gregoire.jadi@univ-nantes.fr>
2015-11-10 16:14:02 +0100

    feat(autoloads): Elfeed* autoloads

---
+++ autoloads/elfeed-org-autoloads.el
@@ -0,0 +1,24 @@
+;;; elfeed-org-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil "../modules/elfeed-org/elfeed-org" "../modules/elfeed-org/elfeed-org.el"
+;;;;;;  (22062 28185 373320 449000))
+;;; Generated autoloads from ../modules/elfeed-org/elfeed-org.el
+
+(autoload 'elfeed-org "../modules/elfeed-org/elfeed-org" "\
+Hook up rmh-elfeed-org to read the `org-mode' configuration when elfeed is run.
+
+\(fn)" t nil)
+
+;;;***
+
+(provide 'elfeed-org-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; elfeed-org-autoloads.el ends here
