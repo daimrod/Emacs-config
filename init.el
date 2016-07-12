0f0af107e44c69bf10711fe75a36946aef79603f
Grégoire Jadi <gregoire.jadi@univ-nantes.fr>
2016-07-12 17:40:34 +0200

    fix(init): Fix small bug with emms and clocking interactions

--- init.el
+++ init.el
@@ -880,8 +880,13 @@ the query (for paths starting with 'query:')."
 (with-eval-after-load 'emms-setup
   (emms-all)
   (emms-default-players)
-  (add-hook 'org-clock-in-hook 'emms-pause)
-  (add-hook 'org-clock-out-hook 'emms-pause))
+  (add-hook 'org-clock-in-hook (lambda ()
+                                 (when (and emms-player-playing-p
+                                            emms-player-paused-p)
+                                            (emms-pause))))
+  (add-hook 'org-clock-out-hook (lambda ()
+                                  (when emms-player-playing-p
+                                    (emms-pause)))))
 
 ;; highlight matching parentheses when the point is on them.
 (show-paren-mode 1)
