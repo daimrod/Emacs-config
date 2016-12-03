8632d89162ec6476c282138960a1a7eb2aad9741
daimrod <daimrod>
2016-12-03 11:44:11 +0100

    fix(dmd-org-check-agenda-file): Always skip missing files

--- elisp/dmd-org-mode.el
+++ elisp/dmd-org-mode.el
@@ -352,18 +352,11 @@ Use it."
   (setq dmd-always-clocking-on nil))
 
 (defun dmd-org-check-agenda-file (file)
-  "Make sure FILE exists.  If not, ask user what to do."
+  "Make sure FILE exists, if not, ignore it."
   (unless (file-exists-p file)
-    (message "Non-existent agenda file %s.  [R]emove from list, [A]bort or [S]kip?"
+    (message "Skip non-existent agenda file %s."
              (abbreviate-file-name file))
-    (let ((r (downcase (read-char-exclusive))))
-      (cond
-       ((equal r ?r)
-        (org-remove-file file)
-        (throw 'nextfile t))
-       ((equal r ?s)
-        (throw 'nextfile t))
-       (t (user-error "Abort"))))))
+    (throw 'nextfile t)))
 
 (provide 'dmd-org-mode)
 
