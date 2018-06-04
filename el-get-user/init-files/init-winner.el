221727a59c0d93c421fb917957759ebee9573871
Grégoire Jadi <gjadi@omecha.info>
2018-06-04 21:01:33 +0200

    Custom changes + init winner-mode

--- el-get-user/init-files/init-winner.el
+++ el-get-user/init-files/init-winner.el
@@ -1,6 +1,5 @@
 ;; Winner configuration
-(with-eval-after-load 'winner
-  (global-set-key (kbd "C-c u") 'winner-undo)
-  (global-set-key (kbd "C-c r") 'winner-redo)
-  (winner-mode 1))
+(winner-mode 1)
+(global-set-key (kbd "C-c u") 'winner-undo)
+(global-set-key (kbd "C-c r") 'winner-redo)
 
