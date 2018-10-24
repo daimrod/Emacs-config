a939df3a957e65254babd5128477839c55f4e565
Grégoire Jadi <gjadi@omecha.info>
2018-10-24 06:59:20 +0200

    fix(init): Remove some packages

--- el-get-user/init-files/init-beacon.el
+++
@@ -1,3 +0,0 @@
-;;; Beacon
-(require 'beacon)
-(beacon-mode 1)
--- el-get-user/init-files/init-winner.el
+++
@@ -1,5 +0,0 @@
-;; Winner configuration
-(winner-mode 1)
-(global-set-key (kbd "C-c u") 'winner-undo)
-(global-set-key (kbd "C-c r") 'winner-redo)
-
